6be34adedb9fe1edf9bf128c87bd5834c6dbdf3a
Maiza Oliveira <maiza.oliveira54@gnail.com>
2020-03-26 16:57:24 -0300

    SwitchCase

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,73 +6,79 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
+        public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            double sumResult = 0;
+            double subtractionResult = 0;
+            double multiplicationResult;
+            double divisionResult;
+            double restResult;
+            double number1RestResult;
+            double number2RestResult;
+            int operação = 5;
+            String nomeOperação;
 
-        double sumResult = 0;
-        double subtractionResult;
-        double multiplicationResult;
-        double divisionResult;
-        double restResult;
-        double number1RestResult;
-        double number2RestResult;
+            double[] numero = new double[2];
+            int arraylength = numero.length;
+            int contador = 0;
+            while (contador < arraylength) {
+                System.out.println("Write a number " + (contador + 1));
+                numero[contador] = Double.parseDouble(reader.readLine());
+                System.out.println("You wrote " + numero[contador]);
+                sumResult = sumResult + numero[contador];
+                subtractionResult = subtractionResult - numero[contador];
 
-        double[] arrayteste = new double[10];
-        int arraylength = arrayteste.length;
-        int contador = 0;
-        while (contador < arraylength) {
-            System.out.println( "Write a number " + (contador+1));
-            arrayteste[contador] = Double.parseDouble(reader.readLine());
-            System.out.println( "You put " + arrayteste[contador]);
-            sumResult = sumResult + arrayteste[contador];
+                contador = contador + 1;
+            }
 
-            contador = contador + 1;
+            System.out.println( "Escolha uma operação:");
+            System.out.println( "0 = sair;");
+            System.out.println( "1 = adição;");
+            System.out.println( "2 = subtração;");
+            System.out.println( "3 = multiplicação;");
+            System.out.println( "4 = divisão;");
+
+            operação = Integer.parseInt(reader.readLine());
+            switch (operação) {
+                case 0: System.out.println( "Você escolheu sair (0), até a próxima." );
+                break;
+                case 1: System.out.println( "Você escolheu adição (1)." );
+                    int contador2 = 0;
+                    System.out.print( "The result of " );
+                    while (contador2 < arraylength) {
+                        System.out.print(numero[contador2]);
+                        contador2 = contador2 + 1;
+                        if (contador2 < arraylength) {
+                            System.out.print( " + ");
+                        }
+                    }
+                    System.out.print( " is " + sumResult);
+                    number1RestResult = numero[0] % 2;
+                    number2RestResult = numero[1] % 2;
+                break;
+                case 2: System.out.println( "Você escolheu subtração (2)" );
+                    int contador3 = 0;
+                    System.out.print( "The result of " );
+                    while (contador3 < arraylength) {
+                        System.out.print(numero[contador3]);
+                        contador3 = contador3 + 1;
+                        if (contador3 < arraylength) {
+                            System.out.print( " - ");
+                        }
+                    }
+                    System.out.print( " is " + subtractionResult);
+                    number1RestResult = numero[0] % 2;
+                    number2RestResult = numero[1] % 2;
+                break;
+                case 3: System.out.println( "Você escolheu multiplicação (3)" );
+                break;
+                case 4: System.out.println( "Você escolheu divisão (4)" );
+                break;
+
+
+            }
         }
-        int contador2 = 0;
-        System.out.print( "The result of " );
-        while (contador2 < arraylength) {
-            System.out.print(arrayteste[contador2] + " + " );
-            contador2 = contador2 + 1;
-            
-        }
-        System.out.print( "is " + sumResult);
-        number1RestResult = arrayteste[0] % 2;
-        number2RestResult = arrayteste[1] % 2;
-
-
-
-        /*
-        subtractionResult = number1 - number2;
-        System.out.println("The result of " + number1 + " - " + number2 + " is " + subtractionResult);
-
-        multiplicationResult = number1 * number2;
-        System.out.println("The result of " + number1 + " x " + number2 + " is " + multiplicationResult);
-
-        divisionResult = number1 / number2;
-        System.out.println("The result of " + number1 + " ÷ " + number2 + " is " + divisionResult);
-
-        restResult = number1 % number2;
-        System.out.println("The result of " + number1 + " % " + number2 + " is " + restResult);
-
-        if (number1 > number2) {
-            System.out.println("First number " + number1 + " is greater than second number " + number2);
-        } else if (number1 < number2) {
-            System.out.println("Second number " + number2 + " is greater than first number " + number1);
-        } else {
-            System.out.println("The first number is the same as the second.");
-        }
-
-        if (number1RestResult == 0) {
-            System.out.println(number1 + " is even.");
-        } else {
-            System.out.println(number1 + " is odd.");
-        }
-        if (number2RestResult != 0) {
-            System.out.println(number2 + " is odd.");
-        } else {
-            System.out.println(number2 + " is even.");
-        }*/
     }
-}
+// tirei lá de cima public static void main(String[] args) throws IOException {
